1100cbf042f65f6e929a5a008f864e81137b9111
Raunit <36741947+raunit-x@users.noreply.github.com>
2024-07-06 19:28:20 +0530

    fix: set code execution to false

--- app/agents/mastery_evaluator.py
+++ app/agents/mastery_evaluator.py
@@ -21,6 +21,7 @@ class MasteryEvaluatorAgent:
             name="assistant",
             llm_config=self.llm_config,
             system_message="You are a helpful assistant specialized in evaluating the mastery level of a student in a topic.",
+            code_execution_config=False
         )
 
         self.user_proxy = autogen.UserProxyAgent(
@@ -35,7 +36,7 @@ class MasteryEvaluatorAgent:
             2. Use your intuition on the subtopics selected and the total subtopics to evaluate the mastery level of the student in the given topic
             3. Generate a mastery level of the user on the given topic from one of these: "beginner" or "intermediate" or "advanced"
             4. Ensure that the mastery level generated belongs to {"beginner", "intermediate", "advanced"}
-            5. End your message with 'TERMINATE'.""",
+            5. End your message with 'TERMINATE'."""
         )
 
     def evaluate_mastery(self, selected_subtopics, total_subtopics) -> list:
--- app/agents/subtopics_generator.py
+++ app/agents/subtopics_generator.py
@@ -21,6 +21,7 @@ class SubtopicsGeneratorAgent:
             name="assistant",
             llm_config=self.llm_config,
             system_message="You are a helpful assistant specialized in breaking down topics into subtopics.",
+            code_execution_config=False
         )
 
         self.user_proxy = autogen.UserProxyAgent(
@@ -34,7 +35,7 @@ class SubtopicsGeneratorAgent:
             1. Generate a list of upto 10 subtopics for the given main topic with each subtopic having a level attached to it (beginner/intermediate/advanced).
             2. Reflect on the generated subtopics and refine them if necessary.
             3. Provide a final, refined list of subtopics in JSON format like this: [{"subtopic": "subtopic1", "level": "beginner"}, {"subtopic": "subtopic2", "level": "intermediate"}, {"subtopic": "subtopic3", "level": "advanced"}]
-            4. End your message with 'TERMINATE'.""",
+            4. End your message with 'TERMINATE'."""
         )
 
     def generate_subtopics(self, main_topic) -> list:
